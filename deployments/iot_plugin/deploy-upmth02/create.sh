61a74ca68b9209e47d7a3d04a9d7b0008e3f85dc
Marcel Wagner <wagmarcel@web.de>
2019-10-06 14:49:59 +0000

    added th0s sensorspec

--- deployments/iot_plugin/deploy-upmth02/create.sh
+++ deployments/iot_plugin/deploy-upmth02/create.sh
@@ -1,15 +1,15 @@
 #NAMESPACE=oisp-devices
 NODENAME=${1:-NODENAME}
 NAMESPACE=${NAMESPACE:-default}
-CONFIG_MAP_NAME=oisp-devices-config
+CONFIG_MAP_NAME=oisp-devices-upm-th02-config
 
 echo Creating subdir for node $NODENAME
 mkdir -p $NODENAME
 echo Copy and adapt the templates
-for yamlfile in $(ls *.yaml); do 
-	echo Processing $yamlfile
-	sed 's|<NODENAME>|'$NODENAME'|g' $yamlfile > $NODENAME/$yamlfile
+for file in $(ls *.yaml *.json); do 
+	echo Processing $file
+	sed 's|<NODENAME>|'$NODENAME'|g' $file > $NODENAME/$file
 done
 #kubectl create namespace ${NAMESPACE} 2>/dev/null || echo "Namespace $NAMESPACE already exists. Continue."
 #kubectl delete configmap ${CONFIG_MAP_NAME} -n ${NAMESPACE} 2>/dev/null || echo "ConfigMap not existing. Continue."
-#kubectl create configmap ${CONFIG_MAP_NAME} --from-file=./config.json.r1 --from-file=./config.json.r2 --from-file=./sensorSpecs.json -n ${NAMESPACE}
+kubectl create configmap ${CONFIG_MAP_NAME}  --from-file=./$NODENAME/sensorSpecs.json -o yaml --dry-run > $NODENAME/oisp-devices-upm-th02-config.yaml 
