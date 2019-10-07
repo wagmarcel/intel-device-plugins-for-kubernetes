7e70a84579aee634fee611d3c29a19ed858cc40e
Marcel Wagner <wagmarcel@web.de>
2019-10-07 10:48:23 +0000

    now with testsensor

---
+++ deployments/iot_plugin/deploy-testsensor/create.sh
@@ -0,0 +1,20 @@
+#NAMESPACE=oisp-devices
+NODENAME=${1:-NODENAME}
+NAMESPACE=${NAMESPACE:-default}
+CONFIG_MAP_NAME=upmth02-config
+
+echo Creating subdir for node $NODENAME
+mkdir -p $NODENAME
+echo Copy and adapt the templates
+for file in $(ls *.yaml *.json); do 
+	echo Processing $file
+	sed 's|<NODENAME>|'$NODENAME'|g' $file > $NODENAME/$file
+done
+#kubectl create namespace ${NAMESPACE} 2>/dev/null || echo "Namespace $NAMESPACE already exists. Continue."
+#kubectl delete configmap ${CONFIG_MAP_NAME} -n ${NAMESPACE} 2>/dev/null || echo "ConfigMap not existing. Continue."
+kubectl create configmap ${CONFIG_MAP_NAME}  --from-file=./$NODENAME/sensorSpecs.json -o yaml --dry-run > $NODENAME/configmap.yaml 
+cat $NODENAME/configmap.yaml > $NODENAME/all.yaml
+echo --- >> $NODENAME/all.yaml
+cat $NODENAME/pvc.yaml >> $NODENAME/all.yaml
+echo --- >> $NODENAME/all.yaml
+cat $NODENAME/deployment.yaml >> $NODENAME/all.yaml
